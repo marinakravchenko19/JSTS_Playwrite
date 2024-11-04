3837b6e2422324077d901a1077ccf88b30183517
Maryna <marynakravchenko19@gmail.com>
2024-11-04 14:19:39 +0100

    Added solution for task 6

---
+++ Homework2/task6.js
@@ -0,0 +1,20 @@
+/**
+Task 6*. Реалізуйте функцію findArrDiff(arr1, arr2), яка приймає числові масиви arr1, arr2 і повертає масив чисел, які не повторюються у вихідних масивах.
+	
+Приклад роботи:
+console.log(difference([5, 10, 20], [0, 10, 20, 30]));		// ["5", "0", "30"]
+ */
+
+findArrDiff = (arr1 = [], arr2 = []) => {
+    let combinedArray = arr1.concat(arr2);
+    let resultArray = [];
+    combinedArray.forEach(el => {
+        if (combinedArray.indexOf(el) == combinedArray.lastIndexOf(el)) {
+            resultArray.push(el);
+        }
+    });
+
+    return resultArray;
+}
+
+console.log(findArrDiff([5, 10, 20], [0, 10, 20, 30]));
